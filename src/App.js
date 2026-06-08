c0cca44f491abeb4be440ec5719d52063723422f
Ezra Duncan <etduncan03@gmail.com>
2026-06-08 11:11:43 +0100

    Add friend form now conditionally renders

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,12 +21,23 @@ const initialFriends = [
   },
 ];
 
+function Button({children, onClick}){
+  return <button className = "button" onClick ={onClick}>{children}</button>
+}
+
 export default function App(){
+  const[showAddFriend, setShowAddFriend] = useState
+  (false);
+
+  function handleShowAddFriend(){
+    setShowAddFriend(show => !show);
+  }
+
   return <div className="app">
     <div className="sidebar">
       <FriendsList />
-      <FormAddFriend />
-      <Button>Add Friend</Button>
+      {showAddFriend && <FormAddFriend />}
+      <Button onClick = {handleShowAddFriend}>{showAddFriend ? "Close" : "Add Friend"}</Button>
       </div>
       <FromSplitBill />
   </div>
@@ -59,9 +72,6 @@ function Friend({friend}){
   </li>
 }
 
-function Button({children}){
-  return <button className = "button">{children}</button>
-}
 
   function FormAddFriend(){
     return <form className="form-add-friend">
