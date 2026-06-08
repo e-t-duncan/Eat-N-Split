3174b6adbcbd086748e5d34eecb284f8dc877f2c
Ezra Duncan <etduncan03@gmail.com>
2026-06-08 11:02:13 +0100

    finished all the static aspects of the project, has all forms, now they just need to be able to dynamically change

--- src/App.js
+++ src/App.js
@@ -23,7 +23,10 @@ export default function App(){
   return <div className="app">
     <div className="sidebar">
       <FriendsList />
+      <FormAddFriend />
+      <Button>Add Friend</Button>
       </div>
+      <FromSplitBill />
   </div>
 }
 
@@ -52,6 +55,47 @@ function Friend({friend}){
           {friend.balance === 0 && <p> 
       You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
   </li>
 }
+
+function Button({children}){
+  return <button className = "button">{children}</button>
+}
+
+  function FormAddFriend(){
+    return <form className="form-add-friend">
+      <label>🧑Friend name</label>
+      <input type = "text" />
+
+      <label>📷Image URL</label>
+      <input type = "text" />
+
+      <Button>Add</Button>
+    </form>
+  }
+
+  function FromSplitBill(){
+    return <form className = "form-split-bill">
+      <h2>Split a bill with friend</h2>
+
+      <label>💰Bill Value</label>
+      <input type = "text" />
+
+      <label>💵Your Expenses</label>
+      <input type = "text" />
+
+      <label>💵There Expenses</label>
+      <input type = "text" disabled/>
+
+      <label>💰Who's paying the bill</label>
+      <select>
+        <option value = "user">You</option>
+        <option value = "friend">Friend</option>
+      </select>
+
+      <Button>Split Bill</Button>
+
+
+    </form>
+  }
